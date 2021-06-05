83b23f2cc3e85d8ca30ce3defe6f625945c596c3
hungshrek <hungshrek@gmail.com>
2021-06-05 19:42:13 +0800

    modi

--- javascript/products.js
+++ javascript/products.js
@@ -30,7 +30,7 @@ const app = createApp({
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
     if (token === '') {
       alert('您尚未登入請重新登入。');
-      window.location = 'login.html';
+      window.location = 'index.html';
     }
     //將 token 放進 header(每次發出請求設定預設值) 
     axios.defaults.headers.common.Authorization = token;
